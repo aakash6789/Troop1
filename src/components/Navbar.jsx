6001a443c3930a72ff2f853001c533b1f5e783ee
Aakash <aakash2506c@gmail.com>
2024-03-12 17:00:13 +0530

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ const Navbar = () => {
     
         <nav className='relative h-[15vh] bg-[#151518] pt-[5vh] text-[#FFFFFF] font-primary font-thin'>
        <img src={troopLogo} className='absolute left-[10%] w-[7%] max-sm:w-[15%] mt-2 '></img>
-        <div className='flex  justify-between  w-2/4 absolute left-[45%] right-[5%]  md:text-[0.85em] sm:text-[0.7em] max-sm:text-[0.4em] '>
+        <div className='flex  justify-between  w-2/4 absolute left-[45%] right-[5%]  md:text-[0.85em] sm:text-[0.7em] max-sm:text-[1vh] '>
         <span className='font-thin py-4'>Home</span>
         <span className='font-thin py-4'>Why us</span>
         <span className='font-thin py-4'>How to play?</span>
         <span className='font-thin py-4'>Contests</span>
         <span className='font-thin py-4'>Blogs</span>
         
-        <button className='text-[#121212] bg-[#ECC960] rounded-full px-[5%] min-sm:px-[3%] py-4 font-semibold'>Download Now</button>
+        <button className='text-[#121212] bg-[#ECC960] rounded-full px-[5%] min-sm:px-[3%] sm:py-4 font-semibold max-sm:w-[25%] max-sm:py-2 max-sm:px-0 max-sm:text-[0.8vh]'>Download Now</button>
 
         </div>  
       </nav>
