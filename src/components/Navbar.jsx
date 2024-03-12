74b81b86d42a14408f58b9de4dfc70b813c9665f
Aakash <aakash2506c@gmail.com>
2024-03-12 17:12:02 +0530

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     
         <nav className='relative h-[15vh] bg-[#151518] pt-[5vh] text-[#FFFFFF] font-primary font-thin'>
        <img src={troopLogo} className='absolute left-[10%] w-[7%] max-sm:w-[15%] mt-2 '></img>
-        <div className='flex  justify-between  w-2/4 max-sm:w-[60%] absolute left-[45%] right-[5%]  md:text-[0.85em] sm:text-[0.7em] max-sm:text-[1vh] '>
+        <div className='flex  justify-between  w-2/4 max-sm:w-[60%] absolute left-[45%] right-[5%] max-sm:left-[40%]  md:text-[0.85em] sm:text-[0.7em] max-sm:text-[1vh] '>
         <span className='font-thin py-4'>Home</span>
         <span className='font-thin py-4'>Why us</span>
         <span className='font-thin py-4'>How to play?</span>
